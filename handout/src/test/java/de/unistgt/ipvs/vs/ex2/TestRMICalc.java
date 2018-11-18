ff926f831c108d43b193405e4548e064d15466b4
giuliom95 <giuliomartella1995@gmail.com>
2018-11-18 16:42:38 +0100

    Basic mono-thread session

--- handout/src/test/java/de/unistgt/ipvs/vs/ex2/TestRMICalc.java
+++ handout/src/test/java/de/unistgt/ipvs/vs/ex2/TestRMICalc.java
@@ -1,6 +1,5 @@
 package de.unistgt.ipvs.vs.ex2;
 
-
 import de.unistgt.ipvs.vs.ex2.client.CalcRmiClient;
 import de.unistgt.ipvs.vs.ex2.client.CalculationMode;
 import de.unistgt.ipvs.vs.ex2.server.CalcRmiServer;
@@ -15,76 +14,76 @@ import org.junit.rules.Timeout;
  * @author bibartoo
  */
 public class TestRMICalc {
-    
-    public Timeout globalTimeout = Timeout.seconds(30);
-    
-    @Test
-    public void test1(){
-        String srvIP   = "localhost";	// "127.0.0.1"
-        int    srvPort = 22345;
-        
-    
-        CalcRmiServer cSrv = new CalcRmiServer(srvIP, "sessionFactoryTest1");
-        cSrv.start();
-        
-        try {
-                Thread.sleep(3000);
-        } catch (InterruptedException e) {
-                e.printStackTrace();
-        }
 
+	public Timeout globalTimeout = Timeout.seconds(30);
 
-        String url = "//" + srvIP + "/sessionFactoryTest1";
-        CalcRmiClient csCli = new CalcRmiClient();
-        csCli.init(url);
+	@Test
+	public void test1() {
+		String srvIP = "localhost"; // "127.0.0.1"
+		int srvPort = 22345;
 
-        
-        Collection<Integer> numbers1_1 = new ArrayList<Integer>(3);
-	numbers1_1.add(1); numbers1_1.add(2); numbers1_1.add(3);
-        csCli.calculate(CalculationMode.ADD, numbers1_1);
+		CalcRmiServer cSrv = new CalcRmiServer(srvIP, srvPort, "sessionFactoryTest1");
+		cSrv.start();
 
-        
-        Collection<Integer> numbers1_2 = new ArrayList<Integer>(3);
-	numbers1_2.add(3); numbers1_2.add(2); numbers1_2.add(1);
-        csCli.calculate(CalculationMode.SUB, numbers1_2);
-                
-        assertEquals(0,csCli.getCalcRes());
-        
-        cSrv.stopServer();
-    }
-    
-    @Test
-    public void test2(){
-        String srvIP   = "localhost";	// "127.0.0.1"
-        int    srvPort = 22345;
-        
-    
-        CalcRmiServer cSrv = new CalcRmiServer(srvIP, "sessionFactoryTest2");
-        cSrv.start();
-        
-        try {
-                Thread.sleep(3000);
-        } catch (InterruptedException e) {
-                e.printStackTrace();
-        }
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
+		String url = "//" + srvIP + ":" + srvPort + "/sessionFactoryTest1";
+		CalcRmiClient csCli = new CalcRmiClient();
+		csCli.init(url);
 
-        String url = "//" + srvIP + "/sessionFactoryTest2";
-        CalcRmiClient csCli = new CalcRmiClient();
-        csCli.init(url);
+		Collection<Integer> numbers1_1 = new ArrayList<Integer>(3);
+		numbers1_1.add(1);
+		numbers1_1.add(2);
+		numbers1_1.add(3);
+		csCli.calculate(CalculationMode.ADD, numbers1_1);
 
-        
-        Collection<Integer> numbers1_1 = new ArrayList<Integer>(3);
-	numbers1_1.add(1); numbers1_1.add(2); numbers1_1.add(3);
-        csCli.calculate(CalculationMode.ADD, numbers1_1);
+		Collection<Integer> numbers1_2 = new ArrayList<Integer>(3);
+		numbers1_2.add(3);
+		numbers1_2.add(2);
+		numbers1_2.add(1);
+		csCli.calculate(CalculationMode.SUB, numbers1_2);
 
-        
-        Collection<Integer> numbers1_2 = new ArrayList<Integer>(3);
-	numbers1_2.add(3); numbers1_2.add(2); numbers1_2.add(1);
-        csCli.calculate(CalculationMode.SUB, numbers1_2);
-                
-        assertEquals(0,csCli.getCalcRes());
-        
-        cSrv.stopServer();
-    }
+		assertEquals(0, csCli.getCalcRes());
+
+		cSrv.stopServer();
+	}
+
+	@Test
+	public void test2() {
+		String srvIP = "localhost"; // "127.0.0.1"
+		int srvPort = 22345;
+
+		CalcRmiServer cSrv = new CalcRmiServer(srvIP, srvPort, "sessionFactoryTest2");
+		cSrv.start();
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String url = "//" + srvIP + ":" + srvPort + "/sessionFactoryTest2";
+		CalcRmiClient csCli = new CalcRmiClient();
+		csCli.init(url);
+
+		Collection<Integer> numbers1_1 = new ArrayList<Integer>(3);
+		numbers1_1.add(1);
+		numbers1_1.add(2);
+		numbers1_1.add(3);
+		csCli.calculate(CalculationMode.ADD, numbers1_1);
+
+		Collection<Integer> numbers1_2 = new ArrayList<Integer>(3);
+		numbers1_2.add(3);
+		numbers1_2.add(2);
+		numbers1_2.add(1);
+		csCli.calculate(CalculationMode.SUB, numbers1_2);
+
+		assertEquals(0, csCli.getCalcRes());
+
+		cSrv.stopServer();
+	}
 }
